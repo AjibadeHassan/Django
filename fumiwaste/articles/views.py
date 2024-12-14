ac7e8f5730fa9b8cf9efd1d2b23d1f3945f60ade
Hassan <hassanajibade17@gmail.com>
2024-12-14 18:58:08 +0100

    template tags

--- fumiwaste/articles/views.py
+++ fumiwaste/articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
 
 # Create your views here.
 def article_list(req):
-    return render(req, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(req, 'articles/article_list.html', {'articles': articles})
