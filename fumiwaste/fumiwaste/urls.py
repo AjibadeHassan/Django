12db7ddbf5a58a611fa21591c1ce8d952af7fe34
Hassan <hassanajibade17@gmail.com>
2024-12-14 20:00:18 +0100

    staticfiles

--- fumiwaste/fumiwaste/urls.py
+++ fumiwaste/fumiwaste/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .import views
 
 urlpatterns = [
@@ -9,3 +10,5 @@ urlpatterns = [
     path('about/',views.about),
     path('',views.home),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
