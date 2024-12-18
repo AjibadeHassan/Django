2f4e260eb2264226273d92c869289f6e56b65376
Hassan <hassanajibade17@gmail.com>
2024-12-19 00:27:46 +0100

    auth user

--- fumiwaste/accounts/views.py
+++ fumiwaste/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 # Create your views here.
 def signup_view(req):
@@ -12,3 +12,12 @@ def signup_view(req):
     else:
         form = UserCreationForm()
     return render(req, 'accounts/signup.html',{'userform': form})
+
+def login_view(req):
+    if req.method == 'POST':
+        form = AuthenticationForm(data = req.POST)
+        if form.is_valid():
+         return redirect('articles:list')
+    else:
+       form = AuthenticationForm()
+    return render(req,'accounts/login.html',{'form': form})
