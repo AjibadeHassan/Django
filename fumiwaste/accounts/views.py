f173c019f89f3060c83319a42441f89a58de97da
Hassan <hassanajibade17@gmail.com>
2024-12-19 14:22:55 +0100

    login redirect

--- fumiwaste/accounts/views.py
+++ fumiwaste/accounts/views.py
@@ -3,32 +3,35 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout
 
 # Create your views here.
-def signup_view(req):
+def signup_view(request):
  
-    if req.method == 'POST':
-        form = UserCreationForm(req.POST)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(req,user)
+            login(request,user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
-    return render(req, 'accounts/signup.html',{'userform': form})
+    return render(request, 'accounts/signup.html',{'userform': form})
 
-def login_view(req):
-    if req.method == 'POST':
-        form = AuthenticationForm(data = req.POST)
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(req,user)
-            return redirect('articles:list')
+            login(request,user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:list')
     else:
        form = AuthenticationForm()
-    return render(req,'accounts/login.html',{'form': form})
+    return render(request,'accounts/login.html',{'form': form})
 
 
-def logout_view(req):
-    if (req.method == 'POST'):
-        logout(req)
+def logout_view(request):
+    if (request.method == 'POST'):
+        logout(request)
         # print('user logged out succesfully')
         return redirect('articles:list')
--- fumiwaste/articles/views.py
+++ fumiwaste/articles/views.py
@@ -5,16 +5,16 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def article_list(req):
+def article_list(request):
     articles = Article.objects.all().order_by('date')
-    return render(req, 'articles/article_list.html', {'articles': articles})
+    return render(request, 'articles/article_list.html', {'articles': articles})
 
 
-def article_details(req,slug):
+def article_details(request,slug):
     print({slug})
     # return HttpResponse( {slug})
     article = Article.objects.get(slug=slug)
-    return render(req,'articles/article_detail.html', {'article': article})
-@login_required(login_url='/accounts/login/')
-def article_create(req):
-    return render(req, 'articles/article_create.html')
+    return render(request,'articles/article_detail.html', {'article': article})
+@login_required(login_url='/accounts/login')
+def article_create(request):
+    return render(request, 'articles/article_create.html')
